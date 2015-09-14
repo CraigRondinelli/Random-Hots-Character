fd5e098fbc06df3af0c17541d81a883eef72e720
CraigRondinelli <rondinelli.1337@gmail.com>
2015-09-14 14:02:23 -0700

    Initial Upload

---
+++ Random HoTS/Random HoTS/Main Window.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Random_HoTS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.picHotsLogo = new System.Windows.Forms.PictureBox();
+            this.lblPapachips = new System.Windows.Forms.Label();
+            this.picGo = new System.Windows.Forms.PictureBox();
+            this.picChar = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picHotsLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picChar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picHotsLogo
+            // 
+            this.picHotsLogo.BackColor = System.Drawing.Color.Transparent;
+            this.picHotsLogo.Image = ((System.Drawing.Image)(resources.GetObject("picHotsLogo.Image")));
+            this.picHotsLogo.Location = new System.Drawing.Point(344, 48);
+            this.picHotsLogo.Name = "picHotsLogo";
+            this.picHotsLogo.Size = new System.Drawing.Size(354, 135);
+            this.picHotsLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picHotsLogo.TabIndex = 0;
+            this.picHotsLogo.TabStop = false;
+            // 
+            // lblPapachips
+            // 
+            this.lblPapachips.AutoSize = true;
+            this.lblPapachips.BackColor = System.Drawing.Color.Transparent;
+            this.lblPapachips.Font = new System.Drawing.Font("Moire", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPapachips.ForeColor = System.Drawing.Color.White;
+            this.lblPapachips.Location = new System.Drawing.Point(336, 0);
+            this.lblPapachips.Name = "lblPapachips";
+            this.lblPapachips.Size = new System.Drawing.Size(408, 45);
+            this.lblPapachips.TabIndex = 1;
+            this.lblPapachips.Text = "Papachips\' Random";
+            // 
+            // picGo
+            // 
+            this.picGo.Location = new System.Drawing.Point(470, 679);
+            this.picGo.Name = "picGo";
+            this.picGo.Size = new System.Drawing.Size(100, 44);
+            this.picGo.TabIndex = 2;
+            this.picGo.TabStop = false;
+            this.picGo.Click += new System.EventHandler(this.picGo_Click);
+            // 
+            // picChar
+            // 
+            this.picChar.BackColor = System.Drawing.Color.Transparent;
+            this.picChar.Image = ((System.Drawing.Image)(resources.GetObject("picChar.Image")));
+            this.picChar.Location = new System.Drawing.Point(103, 120);
+            this.picChar.Name = "picChar";
+            this.picChar.Size = new System.Drawing.Size(695, 541);
+            this.picChar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picChar.TabIndex = 3;
+            this.picChar.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1025, 322);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(982, 741);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picGo);
+            this.Controls.Add(this.lblPapachips);
+            this.Controls.Add(this.picHotsLogo);
+            this.Controls.Add(this.picChar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picHotsLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picChar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picHotsLogo;
+        private System.Windows.Forms.Label lblPapachips;
+        private System.Windows.Forms.PictureBox picGo;
+        private System.Windows.Forms.PictureBox picChar;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Random HoTS/Random HoTS/Main Window.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Random_HoTS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            DoubleBuffered = true;
+
+            this.Text = "Papachips' Random HoTS Picker";
+
+            picChar.Location = new Point (270,189);
+        }
+
+        private void picGo_Click(object sender, EventArgs e)
+        {
+            picChar.Location = new Point (270,189);
+
+            Random randomnumber = new Random();
+            int charselect = randomnumber.Next(1, 2);
+
+            label1.Text = charselect.ToString();
+
+            if(charselect == 1)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Abathur.png");
+                picChar.Location = new Point(130, 120);
+            }
+
+            else if (charselect == 2)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\AnubArak.png");
+                picChar.Location = new Point(130, 120);
+            }
+
+            //else if (charselect == 41)
+            //{
+            //    picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Artanis.png");
+            // picChar.Location = new Point(105, 120);
+            //}
+
+            else if (charselect == 3)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Arthas.png");
+                picChar.Location = new Point(120, 120);
+            }
+
+            else if (charselect == 4)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Azmodan.png");
+                picChar.Location = new Point(134, 120);
+            }
+
+            else if (charselect == 5)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Brightwing.png");
+                picChar.Location = new Point(130, 120);
+            }
+
+            else if (charselect == 6)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Butcher.png");
+                picChar.Location = new Point(106, 120);
+            }
+
+            else if (charselect == 7)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Chen.png");
+                picChar.Location = new Point(141, 120);
+            }
+
+            else if (charselect == 8)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Diablo.png");
+                picChar.Location = new Point(116, 120);
+            }
+
+            else if (charselect == 9)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\ETC.png");
+                picChar.Location = new Point(103, 120);
+            }
+
+            else if (charselect == 10)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Falstad.png");
+            }
+
+            else if (charselect == 11)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Illidan.png");
+            }
+
+            else if (charselect == 12)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Jaina.png");
+            }
+
+            else if (charselect == 13)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Johanna.png");
+            }
+
+            else if (charselect == 14)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\KaelThas.png");
+            }
+
+            else if (charselect == 15)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Kerrigan.png");
+            }
+
+            else if (charselect == 16)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Leoric.png");
+            }
+
+            else if (charselect == 17)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Lili.png");
+            }
+
+            else if (charselect == 18)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\LostVikings.png");
+            }
+
+            else if (charselect == 19)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Malfurion.png");
+            }
+
+            //else if (charselect == 42)
+            //{
+            //    picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Medic.png");
+            //}
+
+            else if (charselect == 20)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Monk.png");
+            }
+
+            else if (charselect == 21)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Muradin.png");
+            }
+
+            else if (charselect == 22)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Murky.png");
+            }
+
+            else if (charselect == 23)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Nazeebo.png");
+            }
+
+            else if (charselect == 24)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Nova.png");
+            }
+
+            else if (charselect == 25)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Raynor.png");
+            }
+
+            else if (charselect == 26)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Rehgar.png");
+            }
+
+            else if (charselect == 43)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Rexxar.png");
+            }
+
+            else if (charselect == 27)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Sg_Hammer.png");
+            }
+
+            else if (charselect == 28)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Sonya.png");
+            }
+
+            else if (charselect == 29)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Stitches.png");
+            }
+
+            else if (charselect == 30)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Sylvanas.png");
+            }
+
+            else if (charselect == 31)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Tassadar.png");
+            }
+
+            else if (charselect == 32)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Thrall.png");
+            }
+
+            else if (charselect == 33)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Tinker.png");
+            }
+
+            else if (charselect == 34)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Tychus.png");
+            }
+
+            else if (charselect == 35)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Tyrael.png");
+            }
+
+            else if (charselect == 36)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Tyrande.png");
+            }
+
+            else if (charselect == 37)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Uther.png");
+            }
+
+            else if (charselect == 38)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Valla.png");
+            }
+
+            else if (charselect == 39)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Zagara.png");
+            }
+
+            else if (charselect == 40)
+            {
+                picChar.ImageLocation = (@"C:\Users\Craig\Desktop\Random Hots\Random HoTS\Random HoTS\bin\Debug\Stock Heroes\Zeratul.png");
+            }
+        }
+    }
+}
